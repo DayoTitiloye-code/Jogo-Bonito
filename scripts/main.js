b4a2f471c674dc122ef0588fc25c1490ce400365
DayoTitiloye-code <DayoTitiloye@outlook.com>
2025-09-01 13:41:45 +0100

    Made ronaldo btn go to ronaldo page

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
 const myHeading = document.querySelector("h1");
+const ronaldoBtn = document.querySelector("button");
 
 myHeading.textContent = "Jogo Bonito";
+
+ronaldoBtn.addEventListener("click", () =>{
+    window.location.replace("../pages/cr7.html")
+})
