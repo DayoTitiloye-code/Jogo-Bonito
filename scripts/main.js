93eef20b644de68b6367706f2bc7459de952c9cd
DayoTitiloye-code <DayoTitiloye@outlook.com>
2025-09-02 17:19:08 +0100

    added event listener on btn

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,13 @@
 const myHeading = document.querySelector("h1");
-const ronaldoBtn = document.querySelector("button");
+const ronaldoBtn = document.querySelector("#cr7");
+const messiBtn = document.querySelector("#messi");
 
 myHeading.textContent = "Jogo Bonito";
 
 ronaldoBtn.addEventListener("click", () =>{
     window.location.replace("../pages/cr7.html")
+})
+
+messiBtn.addEventListener("click", () =>{
+    window.location.replace("../pages/messi.html")
 })
